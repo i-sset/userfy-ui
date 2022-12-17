4a2335f5e7d0b4a65160bfab9cd410526d5d561e
Josset <josset.garcia@ip-192-168-0-5.ap-southeast-1.compute.internal>
2022-12-16 23:06:16 -0300

    add call to update endpoint with axios

--- src/api/UserAPI.js
+++ src/api/UserAPI.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
+const endpoint = "http://localhost:8080";
+
 const UserAPI = {
-    getAll: async () => await axios.get("http://localhost:8080/users")
+    getAll: async () => await axios.get(endpoint + '/users'),
+    update: async (user) => await axios.put(endpoint + '/user/update', user)
 }
 
 export default UserAPI;
--- src/api/UserAPI.test.js
+++ src/api/UserAPI.test.js
@@ -2,13 +2,31 @@ import UserAPI from "./UserAPI";
 import axios from 'axios';
 jest.mock('axios');
 
+const endpoint = "http://localhost:8080";
 describe('UserAPI', () => {
     describe('get all users', () => {
         it('should call get endpoint', async () => {
             await UserAPI.getAll();
 
-            let url = "http://localhost:8080/users";
-            expect(axios.get).toHaveBeenCalledWith(url);
+            let getURL = endpoint + "/users";
+            expect(axios.get).toHaveBeenCalledWith(getURL);
         });
+    });
+
+    describe('update user', () => {
+        it("should call update endpoint", async () => {
+            let user = {
+                id: 10,
+                firstname: 'josset',
+                lastname: 'garcia',
+                email: 'isset.josset@gmail.com',
+                age: 26
+            };
+            let updateURL = endpoint + "/user/update";
+
+            await UserAPI.update(user);
+            expect(axios.put).toHaveBeenCalledWith(updateURL, user);
+        });
+
     });
 });
